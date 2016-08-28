bb08f71d0fd102a0c81dce27a84cdf4d08630f09
Michał Cegielski <cegmichal@gmail.com>
2016-08-28 18:43:18 +0200

    adding rest of the functionality to currencies

--- lib/App/RequestDetailsParser.php
+++ lib/App/RequestDetailsParser.php
@@ -7,6 +7,9 @@ class RequestDetailsParser {
     const DEFAULT_LIMIT = 10;
     const MAX_LIMIT = 100;
     
+    const DEFAULT_SORT_BY = "ID";
+    const DEFAULT_SORT_HOW = "DESC";
+    
     private $requestParams;
     
     public function __construct($request) {
@@ -24,6 +27,13 @@ class RequestDetailsParser {
     public function getOffset(){
         $requestedOffset = @$this->requestParams['offset'] ?: 0;
         return $requestedOffset;
+    }
+    
+    public function getOrder(){
+        $sortBy = @$this->requestParams['sort_by'] ?: self::DEFAULT_SORT_BY;
+        $sortHow = @$this->requestParams['sort_how'] ?: self::DEFAULT_SORT_HOW;
+        
+        return [$sortBy => $sortHow];
     }
     
 }
--- routes/currencies.php
+++ routes/currencies.php
@@ -4,8 +4,10 @@ use App\RequestDetailsParser;
 use App\Currency;
 use App\CurrencyTransformer;
 
+use Exception\NotFoundException;
 
 use League\Fractal\Manager;
+use League\Fractal\Resource\Item;
 use League\Fractal\Resource\Collection;
 use League\Fractal\Serializer\DataArraySerializer;
 
@@ -15,7 +17,7 @@ $app->get("/currencies", function ($request, $response, $arguments) use ($app) {
         ->all([
             "user_id" => $this->token->getUserId()
         ])
-        ->order(["modified" => "DESC"])
+        ->order($requestDetailsParser->getOrder())
         ->limit($requestDetailsParser->getLimit(),$requestDetailsParser->getOffset());
 
     /* Serialize the response data. */
@@ -23,6 +25,63 @@ $app->get("/currencies", function ($request, $response, $arguments) use ($app) {
     $fractal->setSerializer(new DataArraySerializer);
     $resource = new Collection($currencies, new CurrencyTransformer);
     $data = $fractal->createData($resource)->toArray();
+
+    return $response->withStatus(200)
+        ->withHeader("Content-Type", "application/json")
+        ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+});
+
+
+$app->post("/currencies", function ($request, $response, $arguments) {
+    $body = $request->getParsedBody();
+
+    $currency = new Currency($body);
+    $currency->user_id = $this->token->getUserId();
+    $this->spot->mapper("App\Currency")->save($currency);
+
+
+    /* Serialize the response data. */
+    $fractal = new Manager();
+    $fractal->setSerializer(new DataArraySerializer);
+    $resource = new Item($currency, new CurrencyTransformer);
+    $data = $fractal->createData($resource)->toArray();
+    $data["status"] = "ok";
+    $data["message"] = "New currency created";
+
+    return $response->withStatus(201)
+        ->withHeader("Content-Type", "application/json")
+        ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+});
+
+$app->get("/currencies/{id}", function ($request, $response, $arguments) {
+    if (false === $currency = $this->spot->mapper("App\Currency")->get(
+        $arguments["id"]
+    )) {
+        throw new NotFoundException("Currency not found.", 404);
+    };
+
+    /* Serialize the response data. */
+    $fractal = new Manager();
+    $fractal->setSerializer(new DataArraySerializer);
+    $resource = new Item($currency, new CurrencyTransformer);
+    $data = $fractal->createData($resource)->toArray();
+
+    return $response->withStatus(200)
+        ->withHeader("Content-Type", "application/json")
+        ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+});
+
+$app->delete("/currencies/{id}", function ($request, $response, $arguments) {
+    if (false === $currency = $this->spot->mapper("App\Currency")->get(
+        $arguments["id"]
+    )) {
+        throw new NotFoundException("Currency not found.", 404);
+    };
+
+    $this->spot->mapper("App\Currency")->delete($currency);
+
+    $data["status"] = "ok";
+    $data["message"] = "Currency deleted";
 
     return $response->withStatus(200)
         ->withHeader("Content-Type", "application/json")
